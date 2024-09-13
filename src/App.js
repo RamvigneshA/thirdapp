76b35439bf832ba9ac649d52618f87f1be6bfe43
RamvigneshA <ramvigneshnanban@gmail.com>
2024-09-14 00:44:59 +0530

    third  dictionary api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [defi, setdefi] = useState("fellow");
+  const [text, settext] = useState("fellow");
+  const [newt,setnewt] = useState('') ;
+  let item;
+
+
+  getdata();
+  function getdata(val) {
+    const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'+ text;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          item = data;
+          gotit();
+        }
+      });
+  }
+  
+  function gotit() {
+    const def = item[0].meanings[0].definitions[0].definition;
+    setdefi(def);
+  }
+
+  function handle(e) {
+   setnewt(e);
+  }
+
+  function clicked() {
+    settext(newt);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={(e) => handle(e.target.value)}  />
+      <button onClick={()=>clicked()} >click</button>
+      <p>{defi}</p>
     </div>
   );
 }
